be8e047f331641253912e4aa01a39e74094aeb2a
tracy zumbu garcia <tracyzumbu@lilo.org>
2021-01-29 18:08:46 +0100

    changed some variable values, finished to code all elements' form

--- src/components/form/Forms.js
+++ src/components/form/Forms.js
@@ -3,36 +3,66 @@ import { Formik, Form } from "formik";
 import FormItems from "./FormItems";
 import FormikControl from "../../formikSetup/FormikControl";
 
-import numOfPlayers from "../../services/optionForm";
-import validationSchema from "../../formikSetup/ValidationSchema.js"
+import {
+  numOfPlayers,
+  level,
+  timeByRound,
+  wordByRound,
+  roundByGame
+} from "../../services/optionForm";
+import validationSchema from "../../formikSetup/ValidationSchema.js";
 
 console.log("numOfPlayers:", numOfPlayers + validationSchema);
 
 const Forms = () => {
   return (
-    <Formik
-      validationSchema={validationSchema}
-    >
+    <Formik validationSchema={validationSchema}>
       {(formik) => {
-        console.log("formik :" + formik)
+        console.log("formik :" + formik);
         return (
           <Form className="form">
             <FormItems question={"Nombre de partie"}>
-              <FormikControl 
-                control="radio" 
+              <FormikControl
+                control="radio"
                 options={numOfPlayers}
                 label="nombre de joueur"
-                name="numOfPlayers" 
+                name="numOfPlayers"
               />
             </FormItems>
             <FormItems question={"Noms des joueurs"}>
-              <FormikControl 
+              <FormikControl
                 control="input"
                 label="Equipe n°"
                 name="nameOfPlayers"
-              >
-
-              </FormikControl>
+              ></FormikControl>
+            </FormItems>
+            <FormItems question={"Choix du niveau"}>
+              <FormikControl
+                control="radio"
+                options={level}
+                name="level"
+              ></FormikControl>
+            </FormItems>
+            <FormItems question={"Temps par manche"}>
+              <FormikControl
+                control="radio"
+                options={timeByRound}
+                name="timeByRound"
+              ></FormikControl>
+            </FormItems>
+            <FormItems question={"Mots par manche"}>
+              <FormikControl
+                control="radio"
+                options={wordByRound}
+                name="gameTime"
+              ></FormikControl>
+            </FormItems>
+            <FormItems question={"Nombre de manche par partie"}>
+              <FormikControl
+                control="radio"
+                options={roundByGame}
+                name="roundByGame"
+              ></FormikControl>
             </FormItems>
           </Form>
         );
--- src/services/optionForm.js
+++ src/services/optionForm.js
@@ -1,9 +1,36 @@
 
-let numOfPlayers = [
+export const numOfPlayers = [
   { key: "2", value: "deux" },
   { key: "4", value: "quatre" },
   { key: "6", value: "six" },
 ];
 
-export default numOfPlayers;
+export const level = [
+  { key: "Facile", value: "facile" },
+  { key: "Normal", value: "normal" },
+  { key: "Difficile", value: "difficle" },
+]
+
+export const timeByRound = [
+  { key: "30", value: "thirty" },
+  { key: "45", value: "fourtyFive" },
+  { key: "60", value: "sixty" },
+  { key: "90", value: "ninety" }
+]
+
+export const wordByRound = [
+  { key: "5", value: "five" },
+  { key: "6", value: "six" },
+  { key: "7", value: "seven" },
+  { key: "8", value: "eight" },
+  { key: "9", value: "nine" }
+]
+
+export const roundByGame = [
+  { key: "3", value: "three" },
+  { key: "5", value: "five" },
+  { key: "7", value: "seven" }
+]
+
+
 

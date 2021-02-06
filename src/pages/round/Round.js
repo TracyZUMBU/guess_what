8897ca63761099ff3675d8ba0b245b4a61ce8644
tracy zumbu garcia <tracyzumbu@lilo.org>
2021-02-06 14:30:16 +0100

    have styled round component

--- src/pages/round/Round.js
+++ src/pages/round/Round.js
@@ -1,21 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom"
 
 //component
-import TitlePage from "../../components/titlePage/TitlePage"
-import Button from '../../components/button/Button'
+import TitlePage from "../../components/titlePage/TitlePage";
+import Button from "../../components/button/Button";
 
 const Round = (props) => {
-    console.log('props:', props, "location: ", props.location, "state :", props.location.state) ;
-    return (
-        <div className="container container--round">
-            <TitlePage title={"Manche"}/>
-            <h2>Equipe</h2>
-            <div className="button-container">
-            <Button className="primary" text={"Lancer la manche"}/>
-            </div>
-        </div>
-    )
-}
+  console.log(
+    "props:",
+    props,
+    "location: ",
+    props.location,
+    "state :",
+    props.location.state
+  );
+  return (
+    <div className="container container--round">
+      <TitlePage title={"Manche"} />
+      <div className="round__box">
+        <h2 className="round__team">Equipe n°1</h2>
+        <p className="round__name">
+          C'est à Pauline de faire deviner à Clément
+        </p>
+      </div>
+      <div className="button-container">
+        <Link to="">
+          <Button className="primary" text={"Lancer la manche"} />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Round
+export default Round;
